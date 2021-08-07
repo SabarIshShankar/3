eb7ff1336082dcc4282f24e506a3fc145439d407
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-08-07 06:35:36 +0000

    err

--- 03/script.js
+++ 03/script.js
@@ -1,2 +1,26 @@
-const canvar = document.getElementById("bjs");
+const canvas = document.getElementById("bjs");
 const engine = new BABYLON.Engine(canvas, true);
+window.addEventListener("keydown", move, false);
+
+var createScene = function (canvas, engine){
+	var scene = new BABYLON.Scene(engine);
+	camera = new BABYLON.ArcRotateCamera("camera", 0, 0, 0, new BABYLON.Vector3(0, 1, -10), scene);
+	camera.setTarget(BABYLON.Vector3.Zero());
+	camera.attachControl(canvas, true);
+	var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+	light.intensity = 5;
+	var building = BABLYLON.SceneLoader.Append("","yourMesh.glb", scene, function(meshes){
+		scene.createDefaultCameraOrLight(true, true, true);
+	}); return scene;
+};
+function move (event){
+}
+const scene = createScene();
+
+ engine.runRenderLoop(function () {
+	 scene.render();
+ });
+
+ window.addEventListener("resize", function(){
+	 engine.resize();
+ });
