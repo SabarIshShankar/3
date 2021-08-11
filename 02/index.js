c85c9dec04c27da74d30392cd4549f382e1400d9
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-08-11 10:49:05 +0000

    tt

--- 02/index.js
+++ 02/index.js
@@ -416,3 +416,51 @@ function createTree(){
 	return tree;
 }
 
+function blowUpTree(vertices, sides, currentTier, scalarMultiplier, odd){
+	var vertexIndex;
+	var vertexVector = new THREE.Vector3();
+	var midPointVector = vertices[0].clone();
+	var offset;
+	for (var i =0; i < sides; i++){
+		vertexIndex = currentTier * sides + 1;
+		vertexVector = vertices[i + vertexIndex].clone();
+		midPointVector.y = vertexVector.y;
+		offset = vertexVector.sub(midPointVector);
+		if(odd){
+			if(i % 2 === 0){
+				offset.normalize().multiplyScalar(scalarMultiplier / 6);
+				vertices[i+vertexIndex].add(offset);
+			} else {
+				offset.normalize().multiplyScalar(scalarMultiplier);
+				vertices[i+vertexIndex].add(offset);
+				vertices[i + vertexIndex].y = vertices[i + vertexIndex + sides].y + 0.05
+			}
+		} else {
+			if(i %2 !== 0){
+				offset.normalize().multiplyScalar(scalarMultiplier/6);
+				vertices[i+vertexIndex].add(offset);
+			} else{
+				offset.normalize().multiplyScalar(scalarMultiplier);
+				vertices[i + vertexIndex].add(offset);
+				vertices[i + vertexIndex].y = vertices[i + vertexIndex + sides].y + 0.05;
+			}
+		}
+	}
+}
+
+
+function tightenTree(vertices, sides, currentTier){
+	var vertexIndex;
+	var vertextVector = new THREE.Vector3();
+	var midPointVector = vertices[0].clone();
+	var offset;
+	for (var i =0; i< sides; i++){
+		vertexIndex = currentTier * sides + 1;
+		vertextVector = vertices[i + vertexIndex].clone();
+		midPointVector.y = vertexVector.y;
+		offset = vertexVector.sub(midPointVector);
+		offset.normalize().multiplyScalar(0.06);
+		vertices[i + vertexIndex].sub(offset);
+	}
+}
+
