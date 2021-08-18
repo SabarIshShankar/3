2e6c6ee0d8e6e0db12822ed1de21e80419e8349a
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-08-18 15:38:42 +0000

    new btn

---
+++ Clickn/script.js
@@ -0,0 +1,10 @@
+var num = 0;
+
+var cookie = document.getElementById("cookie");
+function cookieClick(){
+	num += 1;
+
+	var numbers = document.getElementById("numbers");
+
+	numbers.innerHTML = num;
+}
